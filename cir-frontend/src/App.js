4c06093cbf88055709eb97fc162bf9f5f1831222
Takele <takele@tern.is>
2026-06-02 13:27:54 +0300

    feat: Add new Impact Reporting feature with crisis dashboard and report form
    - Updated package.json to include Mantine and Leaflet dependencies.
    - Created AddReport component for adding new crisis reports.
    - Refactored App component to use React Router for navigation.
    - Introduced CrisisDashboard2 component for displaying crisis data with a map.
    - Implemented ImpactReportForm component for submitting impact reports.
    - Added MapComponent for geolocation and map interaction.
    - Created utils.js for API configuration with Axios.
    - Enhanced CrisisDashboard to export crisis data for external use.

--- cir-frontend/src/App.js
+++ cir-frontend/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 
-import CrisisDashboard from './/CrisisDashboard';
+import CrisisDashboard2 from './/CrisisDashboard2';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { MantineProvider } from '@mantine/core';
+import ImpactReportForm from './ImpactReportForm';
+
 
 function App() {
-  return (
 
-    <CrisisDashboard />
+  const router = createBrowserRouter([
+    { path: '/', element: <CrisisDashboard2 /> },
+    { path: '/add-report/:id?/:name?', element: <ImpactReportForm /> },
+
+  ]);
+
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterProvider router={router} />
+    </MantineProvider>
   );
 }
 
