7ca41f7eec8bef104d47da73b3fda7364df22bdd
Baskethio <aklogdaniel@gmail.com>
2026-06-24 10:17:42 +0300

    fixes for hosting

--- cir-frontend/src/constants.js
+++ cir-frontend/src/constants.js
@@ -1,2 +1,3 @@
-export const SERVER_IP = "http://196.188.249.109:5000"
+// export const SERVER_IP = "http://196.188.249.109:5000"
+export const SERVER_IP = "https://www.undp-damage-reporting.com/backend";
 // export const SERVER_IP = "http://localhost:8000/"
