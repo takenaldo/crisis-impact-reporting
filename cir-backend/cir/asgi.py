d212629896f3f91b53ff6333aa43659ac48c6347
Mekdim Tamrat <mekdimtamirat2014@gmail.com>
2026-06-26 10:59:59 +0300

    refactor asgi.py to remove duplicate DJANGO_SETTINGS_MODULE setting

--- cir-backend/cir/asgi.py
+++ cir-backend/cir/asgi.py
@@ -1,10 +1,10 @@
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cir.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from api.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cir.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
